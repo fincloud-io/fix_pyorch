e0cd93510a2ba1a3cca2faa541621c643e0d8984
Jon Jenkins <jj@fincloud.io>
2022-05-18 09:22:05 +0100

    Added IOI test case.

--- tests/tests.py
+++ tests/tests.py
@@ -1,3 +1,4 @@
+import json
 import re
 import os
 import yaml
@@ -10,9 +11,11 @@ config = None
 static_test_data = [
     "8=FIX.4.49=7535=A49=ICE34=152=20200323-22:55:02.50041756=11057=498=0108=30141=Y10=253",
     "8=FIX.4.49=5835=049=ICE34=6552=20200323-23:14:03.67247856=11057=410=239",
+    "8=FIXT.1.19=65635=AE34=109049=TRDCAPSOURCE52=20220429-09:55:26.50156=TRDCAPCOMP57=TRDCAPCOMPFIX115=USD17=88888831=8812332=555=PVFV2260=20220429-08:50:0075=20200101150=0423=2568=TrdReq_TRDCAPFIX_20220428-23:24:02571=7d6f27b8-7bc1-4724-b634-3385d0488888748=6828=1854=11003=17788881300=Commodities2343=4552=254=1453=2448=SXYZ447=D452=7448=SXYZ447=D452=30578=TFU336=1625=358=Trace Capture Report Test1057=N2344=454=2453=6448=SQBC447=D452=7448=operations@mybroker.com447=D452=36448=SABC447=D452=30448=CLIENT1447=D452=1448=S777447=D452=4448=XXX447=D452=211=A4 SABC 88888578=TFU336=1625=358=Trader Name1057=N2344=310=079",
+    "8=FIX.4.49=23635=634=47249=FCUAT50=jj@fincloud.io52=20220517-08:53:50.62156=XXUAT142=BROKERX IOI22=523=hvrMWbA7t5Fw2m0CeZjBO3___n8uxsXkbQ27=55028=N44=100.048=VOD.L54=255=VOD.L58=hello desk62=20220517-23:59:59130=Y215=1216=1217=XY10=225"
 ]
 
-test_cases = ["Logon", "Heartbeat"]
+test_cases = ["Logon", "Heartbeat", "TradeCaptureReport", "IOI"]
 
 
 class TestBasicRepositorySpecFunctions(unittest.TestCase):
@@ -27,7 +30,7 @@ class TestBasicRepositorySpecFunctions(unittest.TestCase):
     def setUp(self):
         global config
         if not config:
-            config_location = os.getenv("FIX_PYORCH_TEST_CONFIG", default="config_test.yaml")
+            config_location = os.getenv("FIX_PYORCH_TEST_CONFIG", default="config_test.yml")
             print('Loading config .. {0}'.format(config_location))
             with open(config_location) as file:
                 config = yaml.load(file, Loader=yaml.Loader)
@@ -57,6 +60,7 @@ class TestBasicRepositorySpecFunctions(unittest.TestCase):
     def test_lookup_basic_message_field_specs(self):
         for i, tm in enumerate(static_test_data):
             msg = Message.parse(tm, self.repo)
+            json_msg = json.loads(msg.to_json())
             self.assertEqual(msg.get_field_by_id(35).value_name(), test_cases[i])
 
     def test_message_parsing(self):
